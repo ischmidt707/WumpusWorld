54ebe055c54e93c2cba3d89738573906470a5d5f
nic-dzomba <nicustm@gmail.com>
2021-10-18 13:07:07 -0600

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -7,12 +7,22 @@ class Agent:
         self.safeCells = 0
         self.pathingRoute = 0
         self.boardKnowledge = 0
+        self.rules = []
+
+    def updateKnowldge(self):
+        pass
 
     def inferenceSystem(self):
+        updated = True
+        while updated:
+            updated = False
+            for rule in self.rules:
+                if self.updateKnowledge(rule):
+                    updated = True
         pass
 
     def takeAction(self):
         pass
-    
+
     def generateRoute(self):
         pass
