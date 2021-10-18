5ba46843e9d70a75a24f04b1881d1137325fa376
nic-dzomba <nicustm@gmail.com>
2021-10-18 12:54:32 -0600

    Create Agent.py
    add agent file

---
+++ Agent.py
@@ -0,0 +1,18 @@
+
+class Agent:
+    def __init__(self):
+        self.arrows = 0
+        self.wumpusAlive = 0
+        self.frontierCells = 0
+        self.safeCells = 0
+        self.pathingRoute = 0
+        self.boardKnowledge = 0
+
+    def inferenceSystem(self):
+        pass
+
+    def takeAction(self):
+        pass
+    
+    def generateRoute(self):
+        pass
