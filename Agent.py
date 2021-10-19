e76f2e6d08b28881d911213d47e7426790fe0ac7
nic-dzomba <nicustm@gmail.com>
2021-10-18 19:00:38 -0600

    update
    all

--- Agent.py
+++ Agent.py
@@ -1,8 +1,10 @@
 from WumpWorld import *
+import random
 
 '''
 Agent class, contains all the decision making, knowledge base, etc of the agent
 '''
+
 
 # class to hold simplified implication statements
 class Implies:
@@ -16,23 +18,26 @@ class Agent:
 
     # initialize instance of agent
     def __init__(self, world):
+        self.actions = 0
         self.world = world
         self.arrows = 0
-        self.wumpusAlive = 0
+        self.wumpusDead = 0
         self.frontierCells = set([])
         self.safeCells = set([])
-        self.pathingRoute = 0
-        self.knowledge = [world.size + 2][world.size + 2] # add buffer around world for safe markers
-        self.pos = [1,1]
-        self.direction = 0 # like unit circle, 0 is facing right, 90 up, 180 left, 270 down
-        self.dead = False
+        self.pathingRoute = []
+        self.knowledge = [world.size + 2][world.size + 2]  # add buffer around world for safe markers
+        self.pos = [1, 1]
+        self.direction = 0  # like unit circle, 0 is facing right, 90 up, 180 left, 270 down
+        self.deadbyWumpus = False
+        self.deadbyPit = False
         self.won = False
         self.rules = []
         self.populateRules()
 
-    #populate the initial knowledge base
+    # populate the initial knowledge base
     def initKnowledge(self):
         pass
+
     # populate rules [] with all the rules we are defining
     def populateRules(self):
         # if empty, all adjacent cells are safe
@@ -45,10 +50,6 @@ class Agent:
         self.rules.append(Implies("bump", "wall", "facing"))
         # mark a wall as safe, as it cannot be a wumpus or pit
         self.rules.append(Implies("wall", "safe", "is"))
-        # if safe, not pit
-        self.rules.append(Implies("safe", "pit", "not"))
-        # if safe, not wumpus
-        self.rules.append(Implies("safe", "wumpus", "not"))
 
     def updateKnowledge(self, x, y, term):
         if term not in self.knowledge[x][y]:
@@ -65,14 +66,29 @@ class Agent:
         if rule.type == "and":
             if rule.LHS == percept:
                 if (self.updateKnowledge(x, y + 1, rule.RHS) or
-                    self.updateKnowledge(x, y - 1, rule.RHS) or
-                    self.updateKnowledge(x - 1, y, rule.RHS) or
-                    self.updateKnowledge(x + 1, y, rule.RHS) or
-                    self.updateKnowledge(x, y, rule.RHS)):
+                        self.updateKnowledge(x, y - 1, rule.RHS) or
+                        self.updateKnowledge(x - 1, y, rule.RHS) or
+                        self.updateKnowledge(x + 1, y, rule.RHS) or
+                        self.updateKnowledge(x, y, rule.RHS)):
                     updated = True
         # if type is or, check if only one is still possible
         if rule.type == "or":
-            pass
+            if rule.LHS == percept:
+                templist = [self.knowledge(x, y - 1), self.knowledge(x, y + 1), self.knowledge(x - 1, y),
+                            self.knowledge(x + 1, y)]
+                if templist.count("safe") == 3:
+                    if "safe" not in (self.knowledge(x, y - 1)):
+                        (self.knowledge(x, y - 1)).append(rule.RHS)
+                        updated = True
+                    if "safe" not in (self.knowledge(x, y + 1)):
+                        (self.knowledge(x, y + 1)).append(rule.RHS)
+                        updated = True
+                    if "safe" not in (self.knowledge(x - 1, y)):
+                        (self.knowledge(x - 1, y)).append(rule.RHS)
+                        updated = True
+                    if "safe" not in (self.knowledge(x + 1, y)):
+                        (self.knowledge(x + 1, y)).append(rule.RHS)
+                        updated = True
         # if type is facing apply it only to tile agent is facing
         if rule.type == "facing":
             if rule.LHS == percept:
@@ -86,17 +102,13 @@ class Agent:
                     if self.updateKnowledge(x - 1, y, rule.RHS):
                         updated = True
                 if self.direction == 270:
-                    if self.updateKnowledge(x, y-1, rule.RHS):
+                    if self.updateKnowledge(x, y - 1, rule.RHS):
                         updated = True
-        #if type is is, just check current tile
+        # if type is is, just check current tile
         if rule.type == "is":
             if rule.LHS == percept:
                 if self.updateKnowledge(x, y, rule.RHS):
                     updated = True
-        #if
-        if rule.type == "not":
-            pass
-
         return updated
 
     # iterate through all the rules and update each one until nothing else can be updated with new knowledge
@@ -108,41 +120,125 @@ class Agent:
                 if self.resolve(rule, x, y, percept):
                     updated = True
 
-    def takeAction(self):
-        #check percepts in current spot
+    def takeAction(self, bumped):
+        x = self.pos[0]
+        y = self.pos[1]
+        self.actions += 1
+        # check percepts in current spot
         percept = self.world.perceiveCell(self.pos[0], self.pos[1])
         # check for game ending conditions
         if 'glitter' in percept:
             self.won == True
             return "Won"
-        if 'wumpus' in percept or 'pit' in percept:
-            self.dead == True
-            return "Dead"
-        #run inference system to update knowledge base
+        if 'wumpus' in percept:
+            self.deadbyWumpus == True
+            return "Wumpus"
+        if 'pit' in percept:
+            self.deadbyPit == True
+            return "Pit"
+
+        # if was bumped previous time, add bump to percepts so its properly processed this time
+        if bumped:
+            percept.append("bump")
+
+        bumped = False
+        popped = False
+        # run inference system to update knowledge base
         self.inferenceSystem(self.pos[0], self.pos[1], percept)
-        #add new cells to safelist and frontier list
+        # add new cells to safelist and frontier list
         # we know its safe since we are currently here!
         self.safeCells.append((self.pos[0], self.pos[1]))
 
-        #add to frontierlist, but only if stil unexplored, safe, and not a wall
-        if ("safe" in self.knowledge[self.pos[0]][self.pos[1] + 1]) and ((self.pos[0], self.pos[1] + 1) not in self.safeCells) and "wall" not in self.knowledge[self.pos[0]][self.pos[1] + 1]:
+        # add to frontierlist, but only if stil unexplored, safe, and not a wall
+        if ("safe" in self.knowledge[self.pos[0]][self.pos[1] + 1]) and (
+                (self.pos[0], self.pos[1] + 1) not in self.safeCells) and "wall" not in self.knowledge[self.pos[0]][
+                self.pos[1] + 1]:
             self.frontierCells.append((self.pos[0], self.pos[1] + 1))
-        if ("safe" in self.knowledge[self.pos[0]][self.pos[1] - 1]) and ((self.pos[0], self.pos[1] - 1) not in self.safeCells) and "wall" not in self.knowledge[self.pos[0]][self.pos[1] - 1]:
+        if ("safe" in self.knowledge[self.pos[0]][self.pos[1] - 1]) and (
+                (self.pos[0], self.pos[1] - 1) not in self.safeCells) and "wall" not in self.knowledge[self.pos[0]][
+                self.pos[1] - 1]:
             self.frontierCells.append((self.pos[0], self.pos[1] - 1))
-        if ("safe" in self.knowledge[self.pos[0] - 1][self.pos[1]]) and ((self.pos[0] - 1, self.pos[1]) not in self.safeCells) and "wall" not in self.knowledge[self.pos[0] - 1][self.pos[1]]:
+        if ("safe" in self.knowledge[self.pos[0] - 1][self.pos[1]]) and (
+                (self.pos[0] - 1, self.pos[1]) not in self.safeCells) and "wall" not in self.knowledge[self.pos[0] - 1][
+                self.pos[1]]:
             self.frontierCells.append((self.pos[0] - 1, self.pos[1]))
-        if ("safe" in self.knowledge[self.pos[0] + 1][self.pos[1]]) and ((self.pos[0] + 1, self.pos[1]) not in self.safeCells) and "wall" not in self.knowledge[self.pos[0] + 1][self.pos[1]]:
+        if ("safe" in self.knowledge[self.pos[0] + 1][self.pos[1]]) and (
+                (self.pos[0] + 1, self.pos[1]) not in self.safeCells) and "wall" not in self.knowledge[self.pos[0] + 1][
+                self.pos[1]]:
             self.frontierCells.append((self.pos[0] + 1, self.pos[1]))
 
-        #by default, if multiple safe adjacent options will choose to go up first then
-        pass
+        # first, if known next to wumpus, kill it, then try moving otherwise
+        # by default, if multiple safe adjacent options will choose to go right first then ccw for choosing
+        # if its a wall, note there is a bump there which will be carried into next call
+        if "wumpus" in self.knowledge[self.pos[0] + 1][self.pos[1]]:
+            if self.world.board[self.pos[0] + 1][self.pos[1]] == "wumpus":
+                self.wumpusDead += 1
+                self.arrows -= 1
+                self.world.board[self.pos[0] + 1][self.pos[1]] = "empty"
+                self.knowledge[self.pos[0] + 1][self.pos[1]].remove("wumpus")
+        elif "wumpus" in self.knowledge[self.pos[0]][self.pos[1] + 1]:
+            if self.world.board[self.pos[0]][self.pos[1] + 1] == "wumpus":
+                self.wumpusDead += 1
+                self.arrows -= 1
+                self.world.board[self.pos[0]][self.pos[1] + 1] = "empty"
+                self.knowledge[self.pos[0]][self.pos[1] + 1].remove("wumpus")
+        elif "wumpus" in self.knowledge[self.pos[0] - 1][self.pos[1]]:
+            if self.world.board[self.pos[0] - 1][self.pos[1]] == "wumpus":
+                self.wumpusDead += 1
+                self.arrows -= 1
+                self.world.board[self.pos[0] - 1][self.pos[1]] = "empty"
+                self.knowledge[self.pos[0] - 1][self.pos[1]].remove("wumpus")
+        elif "wumpus" in self.knowledge[self.pos[0]][self.pos[1] - 1]:
+            if self.world.board[self.pos[0]][self.pos[1] - 1] == "wumpus":
+                self.wumpusDead += 1
+                self.arrows -= 1
+                self.world.board[self.pos[0]][self.pos[1] - 1] = "empty"
+                self.knowledge[self.pos[0]][self.pos[1] - 1].remove("wumpus")
+        elif (self.pos[0] + 1, self.pos[1]) in self.frontierCells:
+            self.direction = 0
+            if "bump" not in self.world.perceiveCell(self.pos[0] + 1, self.pos[1]):
+                self.pos[0] += 1
+            else:
+                self.frontierCells.remove((self.pos[0] + 1, self.pos[1]))
+                bumped = True
+        elif (self.pos[0], self.pos[1] + 1) in self.frontierCells:
+            self.direction = 90
+            if "bump" not in self.world.perceiveCell(self.pos[0], self.pos[1] + 1):
+                self.pos[1] += 1
+            else:
+                self.frontierCells.remove((self.pos[0], self.pos[1] + 1))
+                bumped = True
+        elif (self.pos[0] - 1, self.pos[1]) in self.frontierCells:
+            self.direction = 180
+            if "bump" not in self.world.perceiveCell(self.pos[0] - 1, self.pos[1]):
+                self.pos[0] -= 1
+            else:
+                self.frontierCells.remove((self.pos[0] - 1, self.pos[1]))
+                bumped = True
+        elif (self.pos[0], self.pos[1] - 1) in self.frontierCells:
+            self.direction = 270
+            if "bump" not in self.world.perceiveCell(self.pos[0], self.pos[1] - 1):
+                self.pos[1] -= 1
+            else:
+                self.frontierCells.remove((self.pos[0], self.pos[1] - 1))
+                bumped = True
+        elif self.pathingRoute:  # start backtracking if pathingRoute is not empty
+            newspot = self.pathingRoute.pop()
+            self.pos[0] = newspot[0]
+            self.pos[1] = newspot[1]
+            popped = True
+        else:  # if all else fails, just go to a random adjacent cell
+
+        # before moving on, add current cell to pathing route stack if any position has changed and not already backtracking
+        if (x != self.pos[0] or y != self.pos[1]) and not popped:
+            self.pathingRoute.append((self.pos[0], self.pos[1]))
+
+        return bumped
 
     def solve(self):
-        while not (self.won or self.dead):
-            self.takeAction()
-        if self.won:
-            return "Won!"
-        if self.dead:
-            return "Dead"
-    def generateRoute(self):
-        pass
+        bumped = False
+        while not (self.won or self.deadbyWumps or self.deadbyPit):
+            bumped = self.takeAction(bumped)
+
+        # returns if gold found or dead, number of wumpus killed, number of cells explored, and number of actions
+        return self.won, self.dead, self.wumpusDead, len(self.safeCells), self.actions
--- WumpWorld.py
+++ WumpWorld.py
@@ -5,6 +5,7 @@ Game state manager and world generator
 import random
 import copy
 
+
 class WumpWorld:
     def __init__(self, size, P_pit, P_obs, P_wumpus):
         self.sqSize = size
@@ -15,20 +16,20 @@ class WumpWorld:
 
     # problem/world generator function
     def generateProblem(self):
-        startX, startY = [random.randint(0, self.sqSize-1), random.randint(0, self.sqSize-1)]
+        startX, startY = [random.randint(0, self.sqSize - 1), random.randint(0, self.sqSize - 1)]
         self.board[startX][startY] = 'empty'
         goldX, goldY = [startX, startY]
         while [goldX, goldY] == [startX, startY]:
-            goldX, goldY = [random.randint(0, self.sqSize-1), random.randint(0, self.sqSize-1)]
+            goldX, goldY = [random.randint(0, self.sqSize - 1), random.randint(0, self.sqSize - 1)]
         self.board[goldX, goldY] = 'gold'
         for x in range(self.sqSize):
             for y in range(self.sqSize):
                 # make sure not to override the agent's start space or the gold
-                if([x,y] != [startX,startY] and [x,y] != [goldX,goldY]):
+                if ([x, y] != [startX, startY] and [x, y] != [goldX, goldY]):
                     rand = random.random
-                    if(rand < self.P_pit):
+                    if (rand < self.P_pit):
                         self.board[x][y] = 'pit'
-                    elif(rand < (self.P_pit + self.P_obs)):
+                    elif (rand < (self.P_pit + self.P_obs)):
                         self.board[x][y] = 'wall'
                     elif (rand < (self.P_pit + self.P_obs + self.P_wumpus)):
                         self.board[x][y] = 'wumpus'
@@ -55,9 +56,9 @@ class WumpWorld:
             print("\n")
 
     def validCell(self, x, y):
-        if(x < 0 or y < 0):
+        if (x < 0 or y < 0):
             return False
-        elif(x > self.sqSize-1 or y > self.sqSize-1):
+        elif (x > self.sqSize - 1 or y > self.sqSize - 1):
             return False
         else:
             return True
@@ -73,28 +74,28 @@ class WumpWorld:
         'glitter' - gold in cell
         """
 
-        #Assume agent hit a wall if cell is not valid
-        if( (not self.validCell(x,y)) or (self.board[x][y] == 'wall') ):
+        # Assume agent hit a wall if cell is not valid
+        if ((not self.validCell(x, y)) or (self.board[x][y] == 'wall')):
             return ['bump']
 
         # start with an empty list; everything perceived will be added to it
         perception = []
         # list adjacent cells
-        up = [x,y-1]
-        down = [x,y+1]
-        left = [x-1,y]
-        right = [x+1,y]
+        up = [x, y - 1]
+        down = [x, y + 1]
+        left = [x - 1, y]
+        right = [x + 1, y]
 
         # check for gold on actual cell
-        if(self.board[x][y] == 'gold'):
+        if (self.board[x][y] == 'gold'):
             perception.append('glitter')
 
         # listing things sensed in adjacent cells
-        for j,k in [up,down,left,right]:
-            if(self.validCell(j,k)):
-                if(self.board[j,k] == 'wumpus'):
+        for j, k in [up, down, left, right]:
+            if (self.validCell(j, k)):
+                if (self.board[j, k] == 'wumpus'):
                     perception.append('stench')
-                elif(self.board[j,k] == 'pit'):
+                elif (self.board[j, k] == 'pit'):
                     perception.append('breeze')
 
         # check for empty list, label as empty if so
